48a216e0687ccbbe99eef6a082281acceea77aa7
Numa <alvarobarbeira@gmail.com>
2015-08-03 16:58:18 -0300

    Filtered Phenotype association based on selected people

--- PrediXcanAssociation.R
+++ PrediXcanAssociation.R
@@ -1,17 +1,32 @@
+library(dplyr)
 
 print("reading pheno")
 fam = read.table("intermediate/GWAS/GWAS.fam")
-pheno = fam$V6
+fam_colnames <- c("FAMILY","INDIVIDUAL", "PATERNAL", "MATERNAL", "SEX","PHENOTYPE")
+colnames(fam) <- fam_colnames
+
+print("reading filter")
+filter = read.table("intermediate/GWAS/GWAS.filter")
+colnames(filter) <- c("FAMILY", "INDIVIDUAL", "FILTER")
+
 print("reading predicted")
 predicted = read.delim("data/output-DGN-WB.txt")
 genes =  colnames(predicted)
 
+print("Preparing data")
+merged_people = merge(fam, filter)
+merged <- cbind(predicted, merged_people)
+filtered <- merged %>% filter(FILTER==1)
+
+pheno <- filtered$PHENO
+filtered <- subset(filtered, select = genes)
+
 print("running correlation")
 OUT<-NULL
 for (gene in genes) {
-    data <- predicted[[gene]]
+    data <- filtered[[gene]]
     results = coef(summary(lm(pheno ~ data)))[c(2,6,8,4)]
-    line = tmp<-c(gene,results)
+    line = c(gene,results)
     OUT<-rbind(OUT,line)
 }
 colnames(OUT)<-c("gene","beta","z-stat","p-val", "se(beta)")
