55bf16870c7a88e173e2647f83056653065f28ec
Numa <alvarobarbeira@gmail.com>
2015-09-10 16:04:51 -0300

    Added better argument support

--- PrediXcanAssociation.R
+++ PrediXcanAssociation.R
@@ -1,16 +1,36 @@
 library(dplyr)
+library(argparse)
+
+parser <- ArgumentParser(description='Generate PrediXcan association')
+parser$add_argument('--predicted',
+                    help='predicted gene expression',
+                    default='data/output-DGN-WB.txt')
+
+parser$add_argument('--pheno',
+                    help='Path of pheno file, without extension',
+                    default='intermediate/GWAS/GWAS.fam')
+
+parser$add_argument('--filter',
+                    help='fam file filter',
+                    default='intermediate/GWAS/GWAS.filter')
+
+parser$add_argument('--output',
+                    help='output',
+                    default='intermediate/predixcan_s_wb.txt')
+
+arguments <- parser$parse_args(commandArgs(TRUE))
 
 print("reading pheno")
-fam = read.table("intermediate/GWAS/GWAS.fam")
+fam = read.table(arguments$pheno)
 fam_colnames <- c("FAMILY","INDIVIDUAL", "PATERNAL", "MATERNAL", "SEX","PHENOTYPE")
 colnames(fam) <- fam_colnames
 
 print("reading filter")
-filter = read.table("intermediate/GWAS/GWAS.filter")
+filter = read.table(arguments$filter)
 colnames(filter) <- c("FAMILY", "INDIVIDUAL", "FILTER")
 
 print("reading predicted")
-predicted = read.delim("data/output-DGN-WB.txt")
+predicted = read.delim(arguments$predicted)
 genes =  colnames(predicted)
 
 print("Preparing data")
@@ -30,4 +50,4 @@ for (gene in genes) {
     OUT<-rbind(OUT,line)
 }
 colnames(OUT)<-c("gene","beta","z-stat","p-val", "se(beta)")
-write.table(OUT,"intermediate/predixcan_s_wb.txt",col.names=T,row.names=F,quote=F)
+write.table(OUT,arguments$output,col.names=T,row.names=F,quote=F)
